389fb2a2aa4f1f745a6d74b096f7860a1753a797
Minakova <ganna.minakova@cgi.com>
2021-08-15 14:10:44 +0200

    auto focus with react

--- src/CreateToDo.jsx
+++ src/CreateToDo.jsx
@@ -9,11 +9,12 @@ export default function CreateToDo(props) {
     console.log(toDoText)
     return <div className="addTodo">
                 <h2>ToDo hinzufügen:</h2>
-                <FormControl 
+                <FormControl
                     className="inputField" 
                     as="textarea" 
                     aria-label="With textarea" 
-                    placeholder="Tipp ToDo ein..." 
+                    placeholder="Tipp ToDo ein..."
+                    autoFocus
                     onChange={ (e, props) =>{setToDoText(e.target.value)} }/>
                 <Button className="button" variant="primary" onClick={() => props.saveToDoItem(toDoText)} >Speichern</Button>{' '}
                 <Button className="button" variant="secondary" onClick={props.showItemsList}>Abbrechen</Button>{' '}
